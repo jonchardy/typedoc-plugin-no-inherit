b9fe9e540b0d63498e7a8d972f5b4435a1f80c0e
Jon Hardy <jonathan.c.hardy@gmail.com>
2018-11-07 15:05:52 -0500

    Fixes and slightly more robust test example

--- src/plugin.ts
+++ src/plugin.ts
@@ -7,69 +7,73 @@ import { CommentPlugin } from 'typedoc/dist/lib/converter/plugins/CommentPlugin'
 /**
  * A handler that deals with inherited reflections.
  */
-@Component({name: 'no-inherit'})
+@Component({ name: 'no-inherit' })
 export class NoInheritPlugin extends ConverterComponent {
-    /**
-     * A list of classes/interfaces that don't inherit reflections.
-     */
-    private noInherit: DeclarationReflection[];
+  /**
+   * A list of classes/interfaces that don't inherit reflections.
+   */
+  private noInherit: DeclarationReflection[];
 
-    /**
-     * Create a new CommentPlugin instance.
-     */
-    initialize() {
-        this.listenTo(this.owner, {
-            [Converter.EVENT_BEGIN]: this.onBegin,
-            [Converter.EVENT_CREATE_DECLARATION]: this.onDeclaration,
-            [Converter.EVENT_RESOLVE_BEGIN]: this.onBeginResolve
-        });
-    }
-
-    /**
-     * Triggered when the converter begins converting a project.
-     *
-     * @param context  The context object describing the current state the converter is in.
-     */
-    private onBegin(context: Context) {
-      this.noInherit = [];
+  /**
+   * Create a new CommentPlugin instance.
+   */
+  initialize() {
+    this.listenTo(this.owner, {
+      [Converter.EVENT_BEGIN]: this.onBegin,
+      [Converter.EVENT_CREATE_DECLARATION]: this.onDeclaration,
+      [Converter.EVENT_RESOLVE_BEGIN]: this.onBeginResolve
+    });
   }
 
-    /**
-     * Triggered when the converter has created a declaration or signature reflection.
-     *
-     * Add to the list of classes/interfaces that don't inherit docs.
-     *
-     * @param context  The context object describing the current state the converter is in.
-     * @param reflection  The reflection that is currently processed.
-     * @param node  The node that is currently processed if available.
-     */
-    private onDeclaration(context: Context, reflection: Reflection, node?) {
-        // only if it's a DeclarationReflection and has a comment with @noinheritdoc tag
-        if (!(node && reflection instanceof DeclarationReflection &&
-              reflection.kindOf(ReflectionKind.ClassOrInterface) &&
-              reflection.comment && reflection.comment.hasTag('noinheritdoc'))) {
-            return;
-        }
+  /**
+   * Triggered when the converter begins converting a project.
+   *
+   * @param context  The context object describing the current state the converter is in.
+   */
+  private onBegin(context: Context) {
+    this.noInherit = [];
+  }
 
-        this.noInherit.push(reflection);
-        CommentPlugin.removeTags(reflection.comment, 'noinheritdoc');
+  /**
+   * Triggered when the converter has created a declaration or signature reflection.
+   *
+   * Add to the list of classes/interfaces that don't inherit docs.
+   *
+   * @param context  The context object describing the current state the converter is in.
+   * @param reflection  The reflection that is currently processed.
+   * @param node  The node that is currently processed if available.
+   */
+  private onDeclaration(context: Context, reflection: Reflection, node?) {
+    // only if it's a DeclarationReflection and has a comment with @noinheritdoc tag
+    if (!(node && reflection instanceof DeclarationReflection &&
+      reflection.kindOf(ReflectionKind.ClassOrInterface) &&
+      reflection.comment && reflection.comment.hasTag('noinheritdoc'))) {
+      return;
     }
 
-    /**
-     * Triggered when the converter begins resolving a project.
-     *
-     * @param context  The context object describing the current state the converter is in.
-     */
-    private onBeginResolve(context: Context) {
-        if (this.noInherit) {
-            this.noInherit.forEach((reflection: DeclarationReflection) => {
-              reflection.children.forEach((child: Reflection) => {
-                if (child instanceof DeclarationReflection && child.inheritedFrom &&
-                    (!child.overwrites || (child.overwrites && child.overwrites !== child.inheritedFrom))) {
-                  CommentPlugin.removeReflection(context.project, child);
-                }
-              });
-            });
-        }
+    this.noInherit.push(reflection);
+    CommentPlugin.removeTags(reflection.comment, 'noinheritdoc');
+  }
+
+  /**
+   * Triggered when the converter begins resolving a project.
+   *
+   * @param context  The context object describing the current state the converter is in.
+   */
+  private onBeginResolve(context: Context) {
+    if (this.noInherit) {
+      const removals = [];
+      this.noInherit.forEach((reflection: DeclarationReflection) => {
+        reflection.children.forEach((child: Reflection) => {
+          if (child instanceof DeclarationReflection && child.inheritedFrom &&
+            (!child.overwrites || (child.overwrites && child.overwrites !== child.inheritedFrom))) {
+            removals.push(child);
+          }
+        });
+      });
+      removals.forEach((removed: DeclarationReflection) => {
+        CommentPlugin.removeReflection(context.project, removed);
+      });
     }
+  }
 }
--- test/src/basic.ts
+++ test/src/basic.ts
@@ -1,4 +1,11 @@
+/**
+ * Documentation for the Animal class.
+ */
 class Animal {
+  /**
+   * The animal's name
+   */
+  public name: string;
   /**
    * Documentation for move() method.
    */
@@ -8,10 +15,24 @@ class Animal {
 }
 
 /**
- * Documentation for the Dog class. Shouldn't include docs for move().
+ * Documentation for the Mammal class.
+ */
+class Mammal extends Animal {
+  /**
+   * The type of hair the mammal has.
+   */
+  public hairType: string;
+}
+
+/**
+ * Documentation for the Dog class.
  * @noInheritDoc
  */
-class Dog extends Animal {
+class Dog extends Mammal {
+  /**
+   * The breed of the dog.
+   */
+  public breed: string;
   /**
    * Documentation for bark() method.
    */
