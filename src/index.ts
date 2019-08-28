187e2fee45523ada17f5d5c171ceb0370579f043
Jon Hardy <jonathan.c.hardy@gmail.com>
2019-08-28 15:30:13 -0400

    Revert "Update index.ts"
    This reverts commit 4f0dd1ea74489d1e7601e5414a5b17edcc0489fa.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Application } from 'typedoc/dist/lib/application';
 import { NoInheritPlugin } from './plugin';
 
-export = (PluginHost: Application) => {
+module.exports = (PluginHost: Application) => {
   const app = PluginHost.owner;
   app.converter.addComponent('no-inherit', new NoInheritPlugin(app.converter));
 }
