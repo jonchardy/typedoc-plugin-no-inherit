99581396f73d74d34c74fd9f2d82ad681b2738b9
Jon Hardy <jonathan.c.hardy@gmail.com>
2019-08-28 16:40:11 -0400

    1.1.10
    Bump dependencies, make a few minor changes to go along

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Application } from 'typedoc/dist/lib/application';
 import { NoInheritPlugin } from './plugin';
 
-module.exports = (PluginHost: Application) => {
+export = (PluginHost: Application) => {
   const app = PluginHost.owner;
   app.converter.addComponent('no-inherit', new NoInheritPlugin(app.converter));
 }
--- test/spec.ts
+++ test/spec.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import { Application } from 'typedoc/dist/lib/application';
 
 const srcDir = 'test/src';
