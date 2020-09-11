6067a49c5a2a3277cee9061f5e86acc53bb9e35c
Jon Hardy <jonathan.c.hardy@gmail.com>
2020-09-11 10:23:38 -0400

    Fix originalName for Travis CI testing

--- test/pretest.js
+++ test/pretest.js
@@ -8,7 +8,10 @@ fs.readFile('./test/specs/basic.json', 'utf8', function(err, data) {
   }
   var result = data.replace(
     /: ".*\/node_modules\/typescript\/lib\/lib\.es5\.d\.ts"/g,
-    `: "${base}/node_modules/typescript/lib/lib.es5.d.ts"`);
+    `: "${base}/node_modules/typescript/lib/lib.es5.d.ts"`).replace(
+    /: ".*\/test\/src\/basic\.ts"/g,
+    `: "${base}/test/src/basic.ts"`
+  );
 
   fs.writeFile('./test/specs/basic.json', result, 'utf8', function (err) {
      if (err) return console.log(err);
